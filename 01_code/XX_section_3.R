4a766eb32277a84f2f411b78454a2b431ce9971f
Sebastian Andre Zambrano Freyre <SA.ZambranoF@alum.up.edu.pe>
2026-02-18 21:18:56 -0500

    Penúltima versión de la sección 3

--- 01_code/XX_section_3.R
+++ 01_code/XX_section_3.R
@@ -1,6 +1,7 @@
 #Separemos las bases en train y test
 db_train <- bind_rows(lista_tablas[1:7])
 db_test <- bind_rows(lista_tablas[8:10])
+db_total <- bind_rows(lista_tablas)
 
 db_train <- db_train %>% filter(age>=18,
                         ocu == 1,                 # Solo ocupados
@@ -12,6 +13,11 @@ db_test <- db_test %>% filter(age>=18,
                                 y_salary_m > 0,           # Salario positivo
                                 !is.na(sex))
 
+db_total <- db_total %>% filter(age>=18,
+                              ocu == 1,                 # Solo ocupados
+                              y_salary_m > 0,           # Salario positivo
+                              !is.na(sex))
+
 db_train <- db_train %>% mutate(
   log_salary = log(y_salary_m),
   female = 1 - sex,
@@ -19,6 +25,12 @@ db_train <- db_train %>% mutate(
 )
 
 db_test <- db_test %>% mutate(
+  log_salary = log(y_salary_m),
+  female = 1 - sex,
+  age2 = age^2
+)
+
+db_total <- db_total %>% mutate(
   log_salary = log(y_salary_m),
   female = 1 - sex,
   age2 = age^2
@@ -73,16 +85,20 @@ db_test <- db_test %>% mutate(
   femage2 = female * age2
 )
 
+db_total <- db_total %>% mutate(
+  femage = female * age,
+  femage2 = female * age2
+)
 
 ##Nuevos modelos
-modelo_5 <- log_salary ~ female + age + age2 + femage + college + hoursWorkUsual + formal + sizeFirm + oficio
-modelo_6 <- log_salary ~ female + age + age2 + femage + femage2 + college + hoursWorkUsual + formal + sizeFirm + oficio
-modelo_7 <- log_salary ~ female + age + age2 + femage + p6050 + college + hoursWorkUsual + formal + sizeFirm + oficio
-modelo_8 <- log_salary ~ female + age + age2 + maxEducLevel + femage + hoursWorkUsual + formal + sizeFirm + oficio
-modelo_9 <- log_salary ~ female + age + age2 + maxEducLevel + femage + femage2 + hoursWorkUsual + formal + sizeFirm + oficio
-modelo_10 <- log_salary ~ female + age + age2 + maxEducLevel + p7040 + hoursWorkUsual + formal + sizeFirm + oficio
-modelo_11 <- log_salary ~ female + age + age2 + maxEducLevel + p7040 + femage + hoursWorkUsual + formal + sizeFirm + oficio
-modelo_12 <- log_salary ~ female + age + age2 + maxEducLevel + p7040 + femage + femage2 + hoursWorkUsual + formal + sizeFirm + oficio
+modelo_5 <-  log_salary ~ female + age + age2 + college + femage + hoursWorkUsual + formal + sizeFirm + oficio
+modelo_6 <-  log_salary ~ female + age + age2 + college + femage + femage2 + hoursWorkUsual + formal + sizeFirm + oficio
+modelo_7 <-  log_salary ~ female + age + age2 + college + femage + p6050 + hoursWorkUsual + formal + sizeFirm + oficio
+modelo_8 <-  log_salary ~ female + age + age2 + college + femage + femage2 + p6050 + hoursWorkUsual + formal + sizeFirm + oficio
+modelo_9 <-  log_salary ~ female + age + age2 + college + femage + femage2 + p7040 + hoursWorkUsual + formal + sizeFirm + oficio
+modelo_10 <- log_salary ~ female + age + age2 + college + p6050 + p7040 + hoursWorkUsual + formal + sizeFirm + oficio
+modelo_11 <- log_salary ~ female + age + age2 + college + p6050 + p7040 + femage + hoursWorkUsual + formal + sizeFirm + oficio
+modelo_12 <- log_salary ~ female + age + age2 + college + p6050 + p7040 + femage + femage2 + hoursWorkUsual + formal + sizeFirm + oficio
 
 
 
@@ -113,5 +129,65 @@ for (i in 5:12) {
 }
 
 # RMSE finales
-rmse_scores[5:12]
+rmse_scores[1] <- score1
+rmse_scores[2] <- score2
+rmse_scores[3] <- score3
+rmse_scores[4] <- score4
+rmse_scores[1:12]
 
+which.min(rmse_scores)
+rmse_scores[which.min(rmse_scores)]
+
+#Nos quedamos con el modelo 11 :)
+# Ajustar modelo
+m11 <- lm(modelo_11, data = db_total)
+
+# Residuos
+e <- resid(m11)
+
+# Leverage (diagonal de la hat matrix)
+h <- hatvalues(m11)
+
+# LOOCV RMSE
+rmse_loocv <- sqrt(mean((e / (1 - h))^2))
+
+rmse_loocv
+
+
+loo_error <- resid(m11) / (1 - hatvalues(m11))
+
+head(order(abs(loo_error), decreasing = TRUE), 10)
+
+residuos <- resid(m11)
+
+# Observaciones con mayor error absoluto
+head(order(abs(residuos), decreasing = TRUE), 10)
+
+
+# Matriz de diseño
+X <- model.matrix(m11)
+
+# Residuos
+e <- resid(m11)
+
+# Leverage
+h <- hatvalues(m11)
+
+# (X'X)^(-1)
+XtX_inv <- solve(t(X) %*% X)
+
+# Multiplicador escalar por observación
+mult <- e / (1 - h)
+
+# Calcular todas las diferencias beta(-i) - beta en una sola operación
+# t(X) %*% diag(mult)  equivale a multiplicar cada fila de X por mult_i
+beta_diff <- XtX_inv %*% t(X * mult)
+
+# Ahora cada columna es el vector beta(-i) - beta
+# Calculamos norma euclidiana por observación
+
+influence_beta <- sqrt(colSums(beta_diff^2))
+#Normalización de la influencia
+influence_beta <- influence_beta / sqrt(ncol(X))
+# Observaciones más influyentes
+head(order(influence_beta, decreasing = TRUE), 10)
